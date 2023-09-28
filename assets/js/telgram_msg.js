9919a8f4c7ae96d44abe3ecdba350892d7616cd0
Tomin-Joy <tominjk007@gmail.com>
2023-09-28 12:10:41 +0530

    bot

--- assets/js/telgram_msg.js
+++ assets/js/telgram_msg.js
@@ -1,5 +1,4 @@
-env:
-    token=${{ secrets.TELE_BOT }};
+
 function register() {
   var form = document.getElementById("contact");
 
@@ -10,7 +9,7 @@ function register() {
   var sem = document.getElementById("sem").value;
  
   var data = { name: name, emailid: emailid, mob: mob, dept: dept, sem: sem };
-  fetch(`https://api.telegram.org/${token}/sendMessage?chat_id=-1001967873486&text=hello2`)
+  fetch(`https://api.telegram.org/bot5990879490:AAHTlMJrRwC-LRwHEV3FS34T9acm9l-yPmY/sendMessage?chat_id=-1001967873486&text=hello2`)
   console.log(data);
   form.reset();
 }
